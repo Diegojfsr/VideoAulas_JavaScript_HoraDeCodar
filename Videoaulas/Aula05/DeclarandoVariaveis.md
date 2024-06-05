138fc52e69ee17abd00171c86e77953b90083fad
DiegoJfsr <diegojfsr@gmail.com>
2024-06-05 20:18:42 -0300

    Aula05

---
+++ Videoaulas/Aula05/DeclarandoVariaveis.md
@@ -0,0 +1,10 @@
+
+Declarando variaveis
+
+Variaveis
+
+- As variaveis podem armazenar valores.
+- Estes valores podem ser utilizados posteriormente.
+- No JavaScript e possivel criar variaveis de tres formas.
+- Podemos mudar o tipo de variavel livremente(Tipagem Fraca).
+
---
+++ Videoaulas/Aula05/DeclarandoVariaveis/script.js
@@ -0,0 +1,31 @@
+
+var teste = 1;
+teste = "Diego";
+console.log(teste);
+
+var nome = "Joao";
+console.log(nome);
+
+
+var $nome = "ADS";
+var _nome = "ADS";
+console.log($nome);
+console.log(_nome);
+
+
+var nome5 = "ASD5";
+console.log(nome5);
+
+var meuPrimeiroNome = "Diego";
+console.log(meuPrimeiroNome);
+
+
+let testando = 1;
+const ola = 2;
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+meuNome = "Diego";
+console.log(meuNome);
+
