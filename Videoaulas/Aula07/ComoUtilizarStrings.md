9578aa73fccb559baf87bd2dd152c2298aa45061
DiegoJfsr <diegojfsr@gmail.com>
2024-06-08 22:04:37 -0300

    ComoUtilizarStrings

---
+++ Videoaulas/Aula07/ComoUtilizarStrings.md
@@ -0,0 +1,10 @@
+
+Como utilizar strings
+
+Tipo de dados String
+
+- String e o tipo de dado para textos.
+- As strings podem ser escritas com aspas simples e duplas.
+- Um numero entre aspas pode ser considerado uma string.
+- E possivel tambem concatenar strings com o +.
+- Vamos ver isso na pratica.
---
+++ Videoaulas/Aula07/ComoUtilizarStrings/script.js
@@ -0,0 +1,37 @@
+
+var nome = "Diego";
+var sobrenome = "Jefferson";
+
+//Concatena duas variaveis
+var nomeCompleto = nome + "" + sobrenome;
+console.log(nomeCompleto);
+
+//Verificando os tipos nas variaveis
+console.log(nome);
+console.log(typeof nome);
+console.log(typeof "5.2929");
+
+//Escrevendo frases completas com Aspas duplas/simples
+var frase = "Esta e uma frase complexa";
+console.log(frase);
+console.log(typeof frase);
+
+
+//Escevendo direto no html
+document.write("Ola");
+
+
+// Textos com palavras entre aspas
+// Envolver o texto por completo entre aspas simples
+
+document.write('Ele disse: "Ola" ');
+
+//Concatenando duas strings
+console.log("Este numero: " + "432");
+
+//Concatenando string e variavel
+console.log("Este numero: " + nome);
+
+
+
+
