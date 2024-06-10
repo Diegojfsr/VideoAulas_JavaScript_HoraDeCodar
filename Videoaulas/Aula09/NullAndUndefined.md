5d3db8203e7fa4dfa80327fd4dec6c102f90dc6f
DiegoJfsr <diegojfsr@gmail.com>
2024-06-10 18:57:11 -0300

    Aula09

---
+++ Videoaulas/Aula09/NullAndUndefined.md
@@ -0,0 +1,13 @@
+
+Null And Undefined
+
+- undefined e null tambem sao considerados tipos de dados.
+- O null e um tipo de dados que representa um valor.
+- O undefined e um tipo de dados para uma variavel com valor nao atribuido.
+- Vamos ver na pratica.
+
+
+
+
+
+
---
+++ Videoaulas/Aula09/NullAndUndefined/script.js
@@ -0,0 +1,21 @@
+
+//Hoisting - icamento
+//Traz a variavel numero para o topo, mas nao atribui o valor a ela.
+
+console.log(numero);
+
+
+
+var nome = null;
+var sobrenome;
+
+console.log(nome);
+console.log(sobrenome);
+
+nome= "Diego";
+console.log(nome);
+
+
+//Hoisting - icamento
+var numero = 5;
+
