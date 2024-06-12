a3fedc21cf30706e16723bc7f28ba82eeee093b4
DiegoJfsr <diegojfsr@gmail.com>
2024-06-11 21:42:41 -0300

    Aula11

---
+++ AulasDe11A20/Aula11/TipoDadosArray.md
@@ -0,0 +1,6 @@
+
+Tipos de dados: Arrays
+
+- Na verdade os arrays são considerados objetos em JavaScript, porem servem como listas.
+- Podemos ter itens de qualquer tipo de dado tambem.
+- Porem nao por chave e valor e sim por indice.
---
+++ AulasDe11A20/Aula11/TipoDadosArray/script.js
@@ -0,0 +1,29 @@
+
+//Array[] //Objeto{}
+//Criando o array
+var arr = [5, "Diego", true,{teste:1, teste:2}]
+console.log(arr);
+
+
+var arr2 = [2, 3, 4, 5, 6];
+console.log(arr2);
+
+
+// Acessando elementos no array
+console.log(arr[1]);
+console.log(arr2[0]);
+
+
+// Inserir elemento no array
+// inserir o elemento 10 no indice 4
+arr [4] = 10; 
+
+
+// Sobrescrever o elemento no array
+// inserir o elemento Teste no indice 0
+
+arr [0] = "Teste";
+
+console.log(arr);
+console.log(typeof arr);
+
