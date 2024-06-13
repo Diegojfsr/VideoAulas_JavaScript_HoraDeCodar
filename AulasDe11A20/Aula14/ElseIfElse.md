a851462f4b1a1cbce98df1a26d8539dfd8d3ccd7
DiegoJfsr <diegojfsr@gmail.com>
2024-06-12 22:06:08 -0300

    Aula14

---
+++ AulasDe11A20/Aula14/ElseIfElse.md
@@ -0,0 +1,8 @@
+
+Condicional: else e else if
+
+- Caso a instrucao de if seja negativa podemos adicionar else.
+- Que sera outra condicional executada.
+- Podemos entao criar uma bifurcacao no codigo.
+- Ja o else if tem a possibilidade de fazer outra verificacao e adicionar mais um bloco de codigo.
+- Vamos ver na pratica.
---
+++ AulasDe11A20/Aula14/ElseIfElse/script.js
@@ -0,0 +1,43 @@
+
+var nome = "Diego";
+
+if(nome=="Pedro"){
+console.log("O nome dele e Pedro.");
+} else {
+console.log("O nome dele e outro.");
+}
+
+
+if(nome=="Pedro"){
+console.log("O nome dele e Pedro.");
+} else if(nome == "Diego") {
+console.log("O nome dele e Diego.");
+} else {
+console.log("O nome dele e outro.");
+}
+
+
+if(nome=="Pedro"){
+console.log("O nome dele e Pedro.");
+} else if(nome == "Lucas") {
+console.log("O nome dele e Lucas.");
+} else if(nome == "Diego") {
+console.log("O nome dele e Diego.");
+} else {
+console.log("O nome dele e outro.");
+}
+
+
+var idade = 19;
+if(idade>20){
+console.log("Ele pode entrar na festa.");
+} else if(idade>=18){
+console.log("Ele so pode entrar com altorizacao.");
+}
+
+var nome = "Diego";
+if(nome == "Diego"){
+console.log("Sim");
+} else {
+console.log("Nao");
+}
