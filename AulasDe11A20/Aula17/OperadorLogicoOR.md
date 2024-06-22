2f6167c32d91c5bdb79140ee47011341820430e7
DiegoJfsr <diegojfsr@gmail.com>
2024-06-21 23:21:58 -0300

    Aula17

---
+++ AulasDe11A20/Aula17/OperadorLogicoOR.md
@@ -0,0 +1,7 @@
+
+Operador lógico OR (||)
+
+- o operador logico || e conhecido como OR.
+- Ele retorna true caso uma das operacoes retorne verdadeiro.
+- O OR retorna false apenas se duas expressoes sao falsas.
+- Vamos ver na pratica.
---
+++ AulasDe11A20/Aula17/OperadorLogicoOR/script.js
@@ -0,0 +1,45 @@
+
+var idade = 16;
+var nome = "Joao";
+
+if(nome == "Joao" || idade > 14){
+console.log("Pode entrar na aula.");
+} else {
+console.log("Nao pode entrar.");
+}
+
+
+
+var idade = 13;
+var nome = "Pedro";
+
+if(nome == "Joao" || idade > 14){
+console.log("Pode entrar na aula.");
+} else {
+console.log("Nao pode entrar.");
+}
+
+
+var idade = 15;
+var nome = "Pedro";
+
+if(nome == "Joao" || idade > 14){
+console.log("Pode entrar na aula.");
+} else {
+console.log("Nao pode entrar.");
+}
+
+
+
+if(nome == "Joao" && 15 > 20 || 10 == 10){
+console.log("Testando");
+} else {
+console.log("Nao entrou");
+}
+
+if(nome == ("Pedro" || 30 > 20) && 10 == 10){
+console.log("Testando");
+} else {
+console.log("Nao entrou");
+}
+
