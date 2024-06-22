9abd66c6c4920c1155487194f3e3570c1e5f5dba
DiegoJfsr <diegojfsr@gmail.com>
2024-06-22 20:16:45 -0300

    Aula18

---
+++ AulasDe11A20/Aula18/OperadorLogicoNOT.md
@@ -0,0 +1,7 @@
+
+Operador Logico !
+
+- O Operador logico ! e conhecido tambem como NOT.
+- Este operador muda o valor que a expressao retornou.
+- Se recebeu true vira false, se recebeu false vira true.
+- Vamos ver na pratica.
---
+++ AulasDe11A20/Aula18/OperadorLogicoNOT/script.js
@@ -0,0 +1,24 @@
+
+if(!true){
+    console.log("Passou");
+    }
+    
+    if(!false){
+    console.log("Passou");
+    }
+    
+    var nome = "Diego";
+    
+    if(nome == "Diego"){
+    console.log("Ok");
+    }
+    
+    if(!(nome == "Diego")){
+    console.log("Ok");
+    }
+    
+    
+    if(!(nome == "Joao")){
+    console.log("Ok");
+    }
+    
