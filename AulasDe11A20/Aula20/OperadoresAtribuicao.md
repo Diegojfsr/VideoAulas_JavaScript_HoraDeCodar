474280baa8886524eaa85d791eb9175b1551375a
DiegoJfsr <diegojfsr@gmail.com>
2024-06-26 20:01:52 -0300

    Aula20

---
+++ AulasDe11A20/Aula20/OperadoresAtribuicao.md
@@ -0,0 +1,8 @@
+
+Operadores de atribuição
+
+- Temos algumas maneiras de atribuir um valor a uma variavel.
+- As mais utilizadas sao: +=, -=, *=, /=.
+- Basicamente e uma forma resumida da operacao: x=x+y.
+- Em loops tambem e comum utilizar os operadores ++ ou --.
+- Vamos ver na pratica.
---
+++ AulasDe11A20/Aula20/OperadoresAtribuicao/script.js
@@ -0,0 +1,34 @@
+
+var x = 1;
+var y = 2;
+
+console.log(x=x+y);
+
+//soma
+console.log(x+=y);
+
+//subtracao
+console.log(x-=y);
+
+//multiplicacao
+console.log(x*=y);
+
+
+//divisao
+console.log(x/=y);
+
+
+//loops
+console.log(x++);
+console.log(x--);
+
+while(x <= 100){
+console.log(x);
+x *= 2;
+}
+console.log(x);
+
+while(x > 0){
+console.log(x);
+x-=5;
+}
