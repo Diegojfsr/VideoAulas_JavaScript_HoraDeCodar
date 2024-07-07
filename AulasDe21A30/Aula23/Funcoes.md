054d5817e5fc05853c22c27e4193321cad10058b
DiegoJfsr <diegojfsr@gmail.com>
2024-07-06 22:18:05 -0300

    Aula23

---
+++ AulasDe21A30/Aula23/Funcoes.md
@@ -0,0 +1,7 @@
+
+Funcoes 
+
+- Funcoes sao blocos de codigos reutilizaveis.
+- Evitamos a repeticao da logica de um programa em diversas partes do codigo.
+- A funcao precisa ser invocada para ser executada.
+- Vamos ver na pratica.
---
+++ AulasDe21A30/Aula23/Funcoes/script.js
@@ -0,0 +1,32 @@
+
+function primeiraFuncao() {
+    console.log("Hello World das funcoes");
+    }
+    primeiraFuncao();
+    
+    
+    
+    function dizerNome(nome){
+    console.log("o nome é: " + nome);
+    }
+    dizerNome("Diego");
+    
+    var nomeDoBancoDeDados = "Joao";
+    dizerNome(nomeDoBancoDeDados);
+    
+    
+    
+    
+    function soma(a, b){
+    var soma = a + b;
+    return soma;
+    }
+    
+    var somaUm = soma(2, 5);
+    console.log(somaUm);
+    
+    var somaDois = soma(2, 5);
+    console.log(somaDois);
+    
+    console.log(soma(4, 5));
+    
