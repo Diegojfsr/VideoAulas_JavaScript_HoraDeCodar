919497010a68786d129712c75e7131d164461a39
DiegoJfsr <diegojfsr@gmail.com>
2024-07-11 13:34:27 -0300

    Aula24

---
+++ AulasDe21A30/Aula24/AlterandoDomComFor.md
@@ -0,0 +1,4 @@
+
+Alterando o HTML com manipulacao do DOM.
+
+Vamos direto a pratica
---
+++ AulasDe21A30/Aula24/AlterandoDomComFor/script.js
@@ -0,0 +1,27 @@
+
+var lista = ["laranja", "banana", "uva", "goiba"];
+
+var listaUl = document.createElement("ul");
+
+
+var body = document.getElementByTagName("body");
+console.log(body[0]);
+
+
+body[0].appendChild(listaUl);
+
+var listaNoBody = document.getElementsByTagName("ul");
+
+
+console.log(listaNoBody[0]);
+
+for(var = 0; i < list.length; i++) {
+
+var liFor = document.createelement("li");
+
+var textoLi = document.createTextNode(lista[i]);
+
+liFor.appendChild(textoLi);
+listaNoBody[0].appendChild(liFor);
+
+}
